3f9b99cc0576fa41681a709ca96cbdc623a26a42
tlazor <tjlazor@vt.edu>
2017-02-08 02:31:53 -0500

    added small test client to test the server

---
+++ test_client.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import socket
+
+
+host = '192.168.0.100'
+port = 50000
+size = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Queries user for input
+query = input("Enter your question: ")
+
+s.connect((host,port))
+s.send(query.encode())
+
+data = s.recv(size)
+
+print(data)
--- twitter_inquirer_server.py
+++ twitter_inquirer_server.py
@@ -19,27 +19,27 @@ s.bind((host, port))
 s.listen(backlog)
 
 while 1:
-	try:
-		client, address = s.accept()
-		data = client.recv(size)
-		print(b'Data recieved: ' + data)
-		data = data.decode()#json.loads(data.decode())
+    try:
+        client, address = s.accept()
+        data = client.recv(size)
+        print(b'Data recieved: ' + data)
+        data = data.decode()  # json.loads(data.decode())
 
-		# Queries user for input
-		query = data
+        # Queries user for input
+        query = data
 
-		# create a new instance of the wolfram class
-		w = wolfram_alpha.wolfram(appID)
+        # create a new instance of the wolfram class
+        w = wolfram_alpha.wolfram(appID)
 
-		answer = w.search(query)
+        answer = w.search(query)
 
-		# just prints the json returned
-		print(answer)
+        # just prints the json returned
+        print(answer)
 
-		client.send(answer)
-	except Exception as inst:
-		print(type(inst))
-		print(inst.args)
-		print(inst)
-		
-		client.send(b'ERROR')
+        client.send(answer)
+    except Exception as inst:
+        print(type(inst))
+        print(inst.args)
+        print(inst)
+
+        client.send(b'ERROR')
