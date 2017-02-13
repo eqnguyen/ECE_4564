1c8eeff0d46f5e0553cb4bf104fa883aaa2ad5a7
Eric <eqnguyen@vt.edu>
2017-02-13 17:49:54 -0500

    Continuously accepts questions until the user inputs "quit"

--- test_client.py
+++ test_client.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python
 
-import socket, pickle, hashlib
+import socket, pickle, hashlib, sys
 
 
 # Receives all amounts of data from the server
 def recvall(sock):
     buff_size = 1024  # 4 KiB
 
-    sizeofmsg = sock.recv(buff_size)
+    sizeofmsg = int(sock.recv(buff_size))
     print("SIZE: " + str(sizeofmsg))
-    msg = sock.recv(int(sizeofmsg))
+
+    msg = sock.recv(sizeofmsg)
+
+    if len(msg) < sizeofmsg:
+        print("Truncated pickle")
+        errormsg = "ERROR CODE: 2"
+        errortup = (errormsg, hashlib.md5(errormsg.encode()).digest())
+        s.send(pickle.dumps(errortup))
+        msg = recvall(sock)
 
     # while True:
     # part = sock.recv(buff_size)
@@ -20,27 +28,39 @@ def recvall(sock):
     return msg
 
 
-host = '192.168.0.101'
+host = '192.168.0.103'
 port = 50000
 size = 1024
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Queries user for input
 query = input("Enter your question: ")
 
-s.connect((host, port))
+while query != "quit":
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+    except socket.error as message:
+        if s:
+            s.close()
+        print("Unable to open socket: " + str(message))
+        sys.exit(1)
 
-tup = (query, hashlib.md5(query.encode()).digest());
-s.send(pickle.dumps(tup))
+    tup = (query, hashlib.md5(query.encode()).digest())
+    s.send(pickle.dumps(tup))
 
-data = recvall(s)
+    while 1:
+        data = recvall(s)
 
-try:
-    tup = pickle.loads(data)
-    print("Answer: " + tup[0])
-except:
-    print("Pickle failure")
-    errormsg = "ERROR CODE: 2"
-    errortup = (errormsg, hashlib.md5(errormsg.encode()).digest())
-    s.send(errortup)
+        try:
+            tup = pickle.loads(data)
+            print("Answer: " + tup[0])
+            break
+        except Exception as inst:
+            print(inst)
+            print("Pickle failure")
+            errormsg = "ERROR CODE: 2"
+            errortup = (errormsg, hashlib.md5(errormsg.encode()).digest())
+            s.send(pickle.dumps(errortup))
+
+    query = input("Enter your question: ")
+    s.close()
