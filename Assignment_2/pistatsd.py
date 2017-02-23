4a5199a4924334e848a394491633a5867b352923
tlazor <tjlazor@vt.edu>
2017-02-23 02:01:35 -0500

    minor typo

--- Assignment_2/pistatsd.py
+++ Assignment_2/pistatsd.py
@@ -24,7 +24,7 @@ def get_cpu_utils():
 parser = argparse.ArgumentParser(description='Publishes stats on network and CPU utilization to a RabbitMQ broker')
 
 parser.add_argument('-b', required=True, help='IP/named address of the message broker')
-parser.add_argument('-p', type=int, help='Virtual host (defaualt is "/")')
+parser.add_argument('-p', type=int, help='Virtual host (default is "/")')
 parser.add_argument('-c', type=int, help='login:password')
 parser.add_argument('-k', type=int, required=True, help='routing key')
 
