c08838fbcde46b4052a8af95cd1f98cd614bda07
Eric <eqnguyen@vt.edu>
2017-02-23 01:40:44 -0500

    Added rabbit subscription code to main function

--- Assignment_2/pistatsview.py
+++ Assignment_2/pistatsview.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python3
 
 import argparse
+import pika
+
+
+def callback(ch, method, properties, body):
+    print(" [x] %r:%r" % (method.routing_key, body))
 
 
 def main():
@@ -12,6 +17,25 @@ def main():
 
     args = parser.parse_args()
 
+    connection = pika.BlockingConnection(pika.ConnectionParameters(args.b))
+    channel = connection.channel()
 
-if __name__ == "__main__":
-    main()
+    channel.exchange_declare(exchange='pi_utilization', type='direct')
+
+    result = channel.queue_declare(exclusive=True)
+    queue_name = result.method.queue
+
+    routing_keys = ['first', 'second', 'third', 'woah']
+    for key in routing_keys:
+        channel.queue_bind(exchange='host_stats',
+                           queue=queue_name,
+                           routing_key=key)
+
+    channel.basic_consume(callback,
+                          queue=queue_name,
+                          no_ack=True)
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+    if __name__ == "__main__":
+        main()
