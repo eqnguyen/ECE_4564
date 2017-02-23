74807cdf78ef40771bc955c29484003252cd4179
Eric <eqnguyen@vt.edu>
2017-02-23 02:10:12 -0500

    Added comments

--- Assignment_2/pistatsview.py
+++ Assignment_2/pistatsview.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 
-import argparse
-import pika
+import argparse, pika
 
 
 def callback(ch, method, properties, body):
@@ -9,32 +8,34 @@ def callback(ch, method, properties, body):
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-b', required=True, help='IP/named address of the message broker')
-    parser.add_argument('-p', type=int, help='Virtual host (defaualt is "/")')
-    parser.add_argument('-c', type=int, help='login:password')
-    parser.add_argument('-k', type=int, required=True, help='routing key')
+    # Create argument parser
+    parser = argparse.ArgumentParser(description='Publishes stats on network and CPU utilization to a RabbitMQ broker')
 
+    # Add arguments to the parser
+    parser.add_argument('-b', required=True, help='IP/named address of the message broker')
+    parser.add_argument('-p', type=int, help='Virtual host (default is "/")')
+    parser.add_argument('-c', type=int, help='login:password')
+    parser.add_argument('-k', required=True, help='routing key')
+
+    # Store arguments into variable
     args = parser.parse_args()
 
+    # Create connection with rabbitMQ broker
     connection = pika.BlockingConnection(pika.ConnectionParameters(args.b))
     channel = connection.channel()
 
     channel.exchange_declare(exchange='pi_utilization', type='direct')
-
     result = channel.queue_declare(exclusive=True)
+
     queue_name = result.method.queue
 
-    channel.queue_bind(exchange='pi_utilization',
-                       queue=queue_name,
-                       routing_key=args.k)
-
-    channel.basic_consume(callback,
-                          queue=queue_name,
-                          no_ack=True)
+    channel.queue_bind(exchange='pi_utilization', queue=queue_name, routing_key=args.k)
+    channel.basic_consume(callback, queue=queue_name, no_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
+    # Start consuming messages
     channel.start_consuming()
 
-    if __name__ == "__main__":
-        main()
+
+if __name__ == "__main__":
+    main()
