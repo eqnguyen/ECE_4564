8b33c1ec1cdf9e7903f15ef6322bef3b170d1329
Eric <eqnguyen@vt.edu>
2017-02-23 03:04:22 -0500

    Created mongo teset file that produces utilization data

---
+++ Assignment_2/mongo_test.py
@@ -0,0 +1,44 @@
+import pymongo
+import psutil
+
+
+def get_cpu_utils():
+    with open('/proc/stat') as f:
+        fields = [float(column) for column in f.readline().strip().split()[1:]]
+    idle, total = fields[3], sum(fields)
+
+    idle_delta = idle - get_cpu_utils.last_idle
+    total_delta = total - get_cpu_utils.last_total
+
+    get_cpu_utils.last_idle = idle
+    get_cpu_utils.last_total = total
+
+    utilisation = 1.0 - idle_delta / total_delta
+
+    return utilisation
+
+
+bytes_sent, bytes_received = 0, 0
+# simulates a static variable for get_cpu_utils
+get_cpu_utils.last_idle, get_cpu_utils.last_total = 0, 0
+
+msg = {'net': {}, 'cpu_usage': 0}
+
+while 1:
+    # gets the cpu utilization, blocking, runs every second
+    msg['cpu_usage'] = psutil.cpu_percent(interval=1)
+    # gets the network info for each NIC
+    network_io = psutil.net_io_counters(pernic=True)
+    for nic in network_io:
+        bytes_sent_old = bytes_sent
+        bytes_received_old = bytes_received
+
+        bytes_sent = network_io[nic].bytes_sent
+        bytes_received = network_io[nic].bytes_recv
+
+        tx_throughput = bytes_sent - bytes_sent_old
+        rx_throughput = bytes_received - bytes_received_old
+
+        msg['net'][nic] = {'tx': tx_throughput, 'rx': rx_throughput}
+
+        print(msg)
