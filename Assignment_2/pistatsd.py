57e9dafcdb2ec56999fb41ccb179c03da7c3b4ad
tlazor <tjlazor@vt.edu>
2017-02-23 01:22:57 -0500

    added pika code

--- Assignment_2/pistatsd.py
+++ Assignment_2/pistatsd.py
@@ -2,14 +2,24 @@
 
 import argparse, psutil
 from time import sleep
+import pika
+
 
 def get_cpu_utils(last_idle, last_total):
     with open('/proc/stat') as f:
         fields = [float(column) for column in f.readline().strip().split()[1:]]
     idle, total = fields[3], sum(fields)
-    idle_delta, total_delta = idle - last_idle, total - last_total
-    last_idle, last_total = idle, total
+
+    idle_delta = idle - last_idle
+    total_delta = total - last_total
+
+    last_idle = idle
+    last_total = total
+
     utilisation = 1.0 - idle_delta / total_delta
+
+    return utilisation
+
 
 parser = argparse.ArgumentParser(description='Generate stats on network and CPU utilization')
 
@@ -19,6 +29,14 @@ parser.add_argument('-c', type=int, help='login:password')
 parser.add_argument('-k', type=int, required=True, help='routing key')
 
 args = parser.parse_args()
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+    args.b))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+channel.exchange_declare(exchange='host_stats',
+                         type='direct')
 
 bytes_sent = 0
 bytes_received = 0
@@ -40,4 +58,7 @@ while 1:
 
         msg['net'][nic] = {'tx': tx_throughput, 'rx': rx_throughput}
 
+    channel.basic_publish(exchange='host_stats',
+                          routing_key=args.k,
+                          body=msg)
     print(msg)
