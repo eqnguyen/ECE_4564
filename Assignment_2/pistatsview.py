6bf2fca0bb1112be4ae319157774830efef359a2
Eric <eqnguyen@vt.edu>
2017-02-26 21:41:53 -0500

    Added error handling for mismatched dictionary keys

--- Assignment_2/pistatsview.py
+++ Assignment_2/pistatsview.py
@@ -43,33 +43,37 @@ def callback(ch, method, properties, body):
     except:
         print("Error: Could not post to MongoDB")
 
-    input = data['cpu']
+    try:
+        input = data['cpu']
 
-    # Display LED based on threshold and input
-    if input < .25:
-        GPIO.output(chan_list, (False, True, False))  # green
-    elif input < .5:
-        GPIO.output(chan_list, (True, True, False))  # yellow
-    else:
-        GPIO.output(chan_list, (True, False, False))  # red
+        # Display LED based on threshold and input
+        if input < .25:
+            GPIO.output(chan_list, (False, True, False))  # green
+        elif input < .5:
+            GPIO.output(chan_list, (True, True, False))  # yellow
+        else:
+            GPIO.output(chan_list, (True, False, False))  # red
 
-    print('\n%r' % method.routing_key)
+        print('\n%r' % method.routing_key)
 
-    # get max and min cpu usage from Mongo
-    max_cpu = posts.find_one(sort=[('cpu', pymongo.DESCENDING)])['cpu']
-    min_cpu = posts.find_one(sort=[('cpu', pymongo.ASCENDING)])['cpu']
-    print('cpu: \t' + str(data['cpu']) + ' [Hi: ' + str(max_cpu) + ', Lo: ' + str(min_cpu) + ']')
-    for item in data['net']:
-        # get max and min rx/tx from Mongo
-        max_rx = posts.find_one(sort=[('net.' + item + '.rx', pymongo.DESCENDING)])['net'][item]['rx']
-        min_rx = posts.find_one(sort=[('net.' + item + '.rx', pymongo.ASCENDING)])['net'][item]['rx']
-        max_tx = posts.find_one(sort=[('net.' + item + '.tx', pymongo.DESCENDING)])['net'][item]['tx']
-        min_tx = posts.find_one(sort=[('net.' + item + '.tx', pymongo.ASCENDING)])['net'][item]['tx']
+        # get max and min cpu usage from Mongo
 
-        print(item + ':\trx=' + str(data['net'][item]['rx']) + ' B/s ',
-              '[Hi: ' + str(max_rx) + ' B/s, Lo: ' + str(min_rx) + ' B/s], ',
-              'tx=' + str(data['net'][item]['tx']) + ' B/s ',
-              '[Hi: ' + str(max_tx) + ' B/s, Lo: ' + str(min_tx) + ' B/s]')
+        max_cpu = posts.find_one(sort=[('cpu', pymongo.DESCENDING)])['cpu']
+        min_cpu = posts.find_one(sort=[('cpu', pymongo.ASCENDING)])['cpu']
+        print('cpu: \t' + str(data['cpu']) + ' [Hi: ' + str(max_cpu) + ', Lo: ' + str(min_cpu) + ']')
+        for item in data['net']:
+            # get max and min rx/tx from Mongo
+            max_rx = posts.find_one(sort=[('net.' + item + '.rx', pymongo.DESCENDING)])['net'][item]['rx']
+            min_rx = posts.find_one(sort=[('net.' + item + '.rx', pymongo.ASCENDING)])['net'][item]['rx']
+            max_tx = posts.find_one(sort=[('net.' + item + '.tx', pymongo.DESCENDING)])['net'][item]['tx']
+            min_tx = posts.find_one(sort=[('net.' + item + '.tx', pymongo.ASCENDING)])['net'][item]['tx']
+
+            print(item + ':\trx=' + str(data['net'][item]['rx']) + ' B/s ',
+                  '[Hi: ' + str(max_rx) + ' B/s, Lo: ' + str(min_rx) + ' B/s], ',
+                  'tx=' + str(data['net'][item]['tx']) + ' B/s ',
+                  '[Hi: ' + str(max_tx) + ' B/s, Lo: ' + str(min_tx) + ' B/s]')
+    except:
+        pass
 
 
 def main():
@@ -90,7 +94,7 @@ def main():
     if args.c is None:
         args.c = 'guest:guest'
     temp = args.c.split(':')
-    if(len(temp)< 2):
+    if (len(temp) < 2):
         print("Error: credentials must include a :")
         sys.exit(1)
     user = temp[0]
@@ -116,7 +120,7 @@ def main():
         channel.start_consuming()
     except pika.exceptions.ConnectionClosed:
         print("Error: pika connection closed")
-        print ("Try fixing your ip")
+        print("Try fixing your ip")
         sys.exit(1)
     except pika.exceptions.ProbableAuthenticationError:
         print("Error: pika probable authentication error")
