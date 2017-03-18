7ce6c3f9c07e51d667e8c9f7d71ef5ab4544e69b
Eric <eqnguyen@vt.edu>
2017-03-18 12:07:22 -0400

    Added argparser and some TLE code

--- Assignment_3/icu.py
+++ Assignment_3/icu.py
@@ -1,3 +1,42 @@
 #! usr/bin/env python3
 
+import sys
+import argparse
+import datetime
 
+
+def main():
+    # Create argument parser
+    parser = argparse.ArgumentParser(
+        description='Artificial satellite monitor gateway that queries Space-Track and NOAA')
+
+    # Add arguments to the parser
+    parser.add_argument('-z', required=True, help='zipcode of viewing area')
+    parser.add_argument('-s', required=True, help='NORAD ID of satellite to view')
+
+    # Store arguments into variable
+    args = parser.parse_args()
+
+    zipcode = args.z
+    noradId = args.s
+
+    date = datetime.datetime.now()
+
+    # Get TLE orbital elements
+    baseURL = 'https://www.space-track.org'
+
+    # Change these
+    username = 'username'
+    password = 'password'
+
+    d1 = date + datetime.timedelta(days=1)
+
+    print('Connecting...')
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        print('')
+        sys.exit(1)
