f1dae4dde54dda971d79199d71ee5477cd814dc4
Eric <eqnguyen@vt.edu>
2017-03-22 00:30:40 -0400

    Fixed args errors and added my number as parameter for sms notification

--- Assignment_3/event_scheduler.py
+++ Assignment_3/event_scheduler.py
@@ -10,11 +10,10 @@ import RPi.GPIO as GPIO
 chan_list = []
 
 
-def sendText(accountSID, authToken, event):
+def sendText(accountSID, authToken, myNumber, event):
     twilioClient = TwilioRestClient(accountSID, authToken)
     twilioNumber = '+12403033631'
-    myNumber = 'myCellNumber'
-
+    body = 'Test'
     message = twilioClient.messages.create(body=body, from_=twilioNumber, to=myNumber)
 
 
@@ -40,15 +39,15 @@ def flashLED(stop_event):
 def start_alerts():
     stop_event = threading.Event()
 
-    threading.Thread(target=beep, args=stop_event).start()
-    threading.Thread(target=flashLED, args=stop_event).start()
+    threading.Thread(target=beep, kwargs={"stop_event":stop_event}).start()
+    threading.Thread(target=flashLED, kwargs={"stop_event":stop_event}).start()
 
     time.sleep(900)
 
     stop_event.set()
 
 
-def event_scheduler(accountSID, authToken, events):
+def event_scheduler(accountSID, authToken, myNumber, events):
     # Set pin mode to the numbers you can read off the pi
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
@@ -59,10 +58,10 @@ def event_scheduler(accountSID, authToken, events):
 
     s = sched.scheduler(time.time, time.sleep)
     for event in events:
-        # schedule the led/audio alerts
-        s.enterabs(time=event.start, action=start_alerts)
         # schedule the sms alert
-        s.enterabs(time=event.start, action=sendText, argument=[accountSID, authToken, event])
+        s.enterabs(time=event['start'], action=sendText, argument=[accountSID, authToken, myNumber, event], priority=1)
+        # schedule the led/audio alerts
+        s.enterabs(time=event['start'], action=start_alerts, priority=1)
     s.run(blocking=True)
 
     GPIO.cleanup(chan_list)
