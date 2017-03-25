f918536294049e75be3a747650b7de4b42a4d8da
Scoot <scott247@vt.edu>
2017-03-25 23:30:24 +0000

    lots of sat stuff works

--- Assignment_3/icu.py
+++ Assignment_3/icu.py
@@ -6,6 +6,8 @@ import json
 import sys
 import time
 import traceback
+import math
+import ephem
 
 import requests
 from event_scheduler import event_scheduler
@@ -70,12 +72,53 @@ def main():
 
     # The following line of code shows the list of clear days returned and the required lat and long
     print('Found ' + str(len(clear_days)) + ' clear days at ' + str(longitude) + ' ' + str(latitude))
+    print('\n' + zipcode + ' Coordinates: \nLongitude: ' + str(longitude) + '\nLatitude: ' + str(latitude))
 
     # Get current date and time
     date = datetime.datetime.now()
 
     # Get TLE orbital elements
     base_url = 'https://www.space-track.org'
+    TLE = []
+
+    try:
+        s = requests.Session()
+        payload = {'identity': username, 'password': password}
+        r = s.post(base_url + '/ajaxauth/login', data=payload)
+        r = s.get('https://www.space-track.org/basicspacedata/query/class/tle_latest/NORAD_CAT_ID/25544/orderby/ORDINAL asc/limit/1/metadata/false')
+        #r = s.get(base_url + '/basicspacedata/query/class/tle_latest/NORAD_CAT_ID/" + norad_id + "/ORDINAL/1/        
+        parsed = r.json()
+        TLE.append((parsed[0]['TLE_LINE0']))
+        TLE.append((parsed[0]['TLE_LINE1']))
+        TLE.append((parsed[0]['TLE_LINE2']))
+        print('\nSatellite TLE: \n' +TLE[0] + '\n' + TLE[1] + '\n' + TLE[2] + '\n')
+
+
+    except:        
+        print("\nError querying space api\nDisplaying trace:\n\n")
+        print(traceback.format_exc())
+        sys.exit(1)
+
+#pi ephem
+
+    iss = ephem.readtle(TLE[0],TLE[1],TLE[2])
+
+    obs = ephem.Observer()
+    obs.lat = latitude
+    obs.long = longitude
+    for p in range(3):
+        tr, azr, tt, altt, ts, azs = obs.next_pass(iss)
+        print('Date/Time (UTC)       Alt/Azim      Lat/Long     Elev')
+        print ('======================================================')
+        while tr < ts :
+            obs.date = tr
+            iss.compute(obs)
+            print(str(tr) + ' | {:4.1f} {:5.1f} | {:4.1f} {:+6.1f} | {:5.1f}'.format(math.degrees(iss.alt), math.degrees(iss.az), math.degrees(iss.sublat), math.degrees(iss.sublong), iss.elevation/1000.))
+            tr = ephem.Date(tr + 60.0 * ephem.second)
+        print('')
+        obs.date = tr + ephem.minute
+
+    print('here')
 
     d1 = date + datetime.timedelta(days=1)
 
