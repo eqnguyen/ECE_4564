788209e942fbfedf0a197487685a9397147004a9
eqnguyen <eqnguyen@vt.edu>
2017-04-06 01:00:37 -0400

    Removed unnecessary time resource and reformatted payload

--- Assignment_4/MineServer.py
+++ Assignment_4/MineServer.py
@@ -1,8 +1,6 @@
 #! /usr/bin/env python3
 
 import asyncio
-import datetime
-import logging
 import pickle
 
 import aiocoap
@@ -12,54 +10,25 @@ import aiocoap.resource as resource
 class PositionResource(resource.Resource):
     def __init__(self):
         super(PositionResource, self).__init__()
-        self.content = (0, 0, 0, 1)
+        self.content = (0, 0, 0, 0)
 
     async def render_get(self, request):
         return aiocoap.Message(payload=pickle.dumps(self.content))
 
     async def render_put(self, request):
-        # Payload format: (token, x, y, z, block_id)
+        # Payload format: (x, y, z, token, block_id)
         payload = pickle.loads(request.payload)
-        if payload[0] < 3:
-            payload[0] += 1
-        else:
-            payload[0] = 1
-        self.content = (payload(1), payload(2), payload(3), payload(0))
+
+        token = (payload[3] + 1) % 3
+
+        self.content = (payload[0], payload[1], payload[3], token)
         payload = ('New payload: %r' % (self.content,)).encode('utf8')
         return aiocoap.Message(payload=payload)
-
-
-class TimeResource(resource.ObservableResource):
-    def __init__(self):
-        super(TimeResource, self).__init__()
-        self.notify()
-
-    def notify(self):
-        self.updated_state()
-        asyncio.get_event_loop().call_later(6, self.notify)
-
-    def update_observation_count(self, count):
-        if count:
-            # not that it's actually implemented like that here -- unconditional updating works just as well
-            print("Keeping the clock nearby to trigger observations")
-        else:
-            print("Stowing away the clock until someone asks again")
-
-    async def render_get(self, request):
-        payload = datetime.datetime.now().strftime("%Y-%m-%d %H:%M").encode('ascii')
-        return aiocoap.Message(payload=payload)
-
-
-# Logging setup
-logging.basicConfig(level=logging.INFO)
-logging.getLogger("coap-server").setLevel(logging.DEBUG)
 
 
 def main():
     # Resource tree creation
     root = resource.Site()
-
-    root.add_resource(('time',), TimeResource())
 
     root.add_resource(('position',), PositionResource())
 
