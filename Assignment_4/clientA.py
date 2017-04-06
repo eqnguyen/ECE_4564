cc87fc93918e06bdcae8554b69f7e53ee8dd51e6
eqnguyen <eqnguyen@vt.edu>
2017-04-06 05:06:05 -0400

    Added loops until wall is built

--- Assignment_4/clientA.py
+++ Assignment_4/clientA.py
@@ -12,41 +12,45 @@ block_id = block.WOOD.id  # Wood
 
 
 async def main():
-    parser = argparse.ArgumentParser(
-        description='Minecraft client that communicates with server to set blocks and build a wall')
+    parser = argparse.ArgumentParser()
     parser.add_argument("ip_address")
     args = parser.parse_args()
 
     protocol = await Context.create_client_context()
 
-    # Get position from server
-    request = Message(code=GET, uri='coap://%s/position' % args.ip_address)
+    while True:
+        # Get position from server
+        request = Message(code=GET, uri='coap://%s/position' % args.ip_address)
 
-    try:
-        response = await protocol.request(request).response
-    except Exception as e:
-        print('Failed to fetch resource:')
-        print(e)
-    else:
-        tup = pickle.loads(response.payload)
-        print('\nGET Minecraft player position')
-        print('Result: %s\n%r' % (response.code, tup))
+        try:
+            response = await protocol.request(request).response
+        except Exception as e:
+            print('Failed to fetch resource:')
+            print(e)
+        else:
+            tup = pickle.loads(response.payload)
+            print('\nGET Minecraft player position')
+            print('Result: %s\n%r' % (response.code, tup))
 
-    # Check for valid token
-    if tup[3] == token:
-        # Payload format: (x, y, z, token, block_id)
-        put_payload = (tup[0] + 1, tup[1], tup[2], token, block_id)
-        print('\nPUT payload: %s' % (put_payload,))
-        payload = pickle.dumps(put_payload)
+        if tup[0] == 'Complete':
+            print('Wall is complete')
+            break
 
-        request = Message(code=PUT, payload=payload)
-        request.opt.uri_host = '192.168.1.10'
-        request.opt.uri_path = ('position',)
+        # Check for valid token
+        if tup[3] == token:
+            # Payload format: (x, y, z, token, block_id)
+            put_payload = (tup[0] + 1, tup[1], tup[2], token, block_id)
+            print('\nPUT payload: %s' % (put_payload,))
+            payload = pickle.dumps(put_payload)
 
-        response = await protocol.request(request).response
+            request = Message(code=PUT, payload=payload)
+            request.opt.uri_host = '192.168.1.10'
+            request.opt.uri_path = ('position',)
 
-        print('PUT Minecraft player position')
-        print('Result: %s\n%r\n' % (response.code, response.payload))
+            response = await protocol.request(request).response
+
+            print('PUT Minecraft player position')
+            print('Result: %s\n%r\n' % (response.code, response.payload))
 
 
 if __name__ == "__main__":
--- Assignment_4/clientB.py
+++ Assignment_4/clientB.py
@@ -12,41 +12,51 @@ block_id = block.DIAMOND_BLOCK.id  # Diamond Block
 
 
 async def main():
-    parser = argparse.ArgumentParser(
-        description='Minecraft client that communicates with server to set blocks and build a wall')
+    parser = argparse.ArgumentParser()
     parser.add_argument("ip_address")
     args = parser.parse_args()
 
     protocol = await Context.create_client_context()
 
-    # Get position from server
-    request = Message(code=GET, uri='coap://%s/position' % args.ip_address)
+    blocks = 0
 
-    try:
-        response = await protocol.request(request).response
-    except Exception as e:
-        print('Failed to fetch resource:')
-        print(e)
-    else:
-        tup = pickle.loads(response.payload)
-        print('\nGET Minecraft player position')
-        print('Result: %s\n%r' % (response.code, tup))
+    while True:
+        # Get position from server
+        request = Message(code=GET, uri='coap://%s/position' % args.ip_address)
 
-    # Check for valid token
-    if tup[3] == token:
-        # Payload format: (x, y, z, token, block_id)
-        put_payload = (tup[0] + 1, tup[1], tup[2], token, block_id)
-        print('\nPUT payload: %s' % (put_payload,))
-        payload = pickle.dumps(put_payload)
+        try:
+            response = await protocol.request(request).response
+        except Exception as e:
+            print('Failed to fetch resource:')
+            print(e)
+        else:
+            tup = pickle.loads(response.payload)
+            print('\nGET Minecraft player position')
+            print('Result: %s\n%r' % (response.code, tup))
 
-        request = Message(code=PUT, payload=payload)
-        request.opt.uri_host = '192.168.1.10'
-        request.opt.uri_path = ('position',)
+        if tup[0] == 'Complete':
+            print('Wall is complete')
+            break
 
-        response = await protocol.request(request).response
+        # Check for valid token
+        if tup[3] == token:
+            # Payload format: (x, y, z, token, block_id)
+            if blocks == 3:
+                put_payload = (tup[0] - 9, tup[1] + 1, tup[2], token, block_id)
+            else:
+                put_payload = (tup[0] + 1, tup[1], tup[2], token, block_id)
 
-        print('PUT Minecraft player position')
-        print('Result: %s\n%r\n' % (response.code, response.payload))
+            print('\nPUT payload: %s' % (put_payload,))
+            payload = pickle.dumps(put_payload)
+
+            request = Message(code=PUT, payload=payload)
+            request.opt.uri_host = '192.168.1.10'
+            request.opt.uri_path = ('position',)
+
+            response = await protocol.request(request).response
+
+            print('PUT Minecraft player position')
+            print('Result: %s\n%r\n' % (response.code, response.payload))
 
 
 if __name__ == "__main__":
