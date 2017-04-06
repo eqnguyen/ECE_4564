7359e351a4f7372b01b560207dd6b71c50831c84
eqnguyen <eqnguyen@vt.edu>
2017-04-06 13:26:20 -0400

    added LED feature

--- Assignment_4/MineServer.py
+++ Assignment_4/MineServer.py
@@ -8,22 +8,25 @@ import aiocoap
 import aiocoap.resource as resource
 from mcpi.minecraft import Minecraft
 
+import RPi.GPIO as GPIO
+
 mc = Minecraft.create()
 
 
 class PositionResource(resource.Resource):
     def __init__(self):
         super(PositionResource, self).__init__()
-        self.init_pos = mc.player.getPos()
+        self.init_pos = mc.player.getTilePos()
         print('Initial position:', self.init_pos.x, self.init_pos.y, self.init_pos.z)
         self.content = (self.init_pos.x, self.init_pos.y, self.init_pos.z, 0)
 
     async def render_get(self, request):
-        pos = mc.player.getPos()
+        pos = mc.player.getTilePos()
 
         # Check if wall is complete
         if pos.x == self.init_pos.x + 10 and pos.y == self.init_pos.y + 1 and pos.z == self.init_pos.z:
             token = 3
+            GPIO.output(chan_list, (0, 0, 0))
             print('Wall is complete')
             mc.postToChat('Wall is complete')
         else:
@@ -37,13 +40,16 @@ class PositionResource(resource.Resource):
         payload = pickle.loads(request.payload)
 
         # Update player position (x, y, z)
-        mc.player.setPos(payload[0], payload[1], payload[2])
+        mc.player.setTilePos(payload[0], payload[1], payload[2])
 
         # Set block at payload location with block_id
         mc.setBlock(payload[0], payload[1], payload[2], payload[4])
 
         # Set token for next player
         token = (payload[3] + 1) % 3
+	
+        # Change LED color to indicate which client's turn it is
+        GPIO.output(chan_list, (token==0, token==1, token==2))
 
         # Send PUT response
         self.content = (payload[0], payload[1], payload[2], token)
@@ -53,6 +59,15 @@ class PositionResource(resource.Resource):
 
 
 def main():
+    # Set pin mode to the numbers you can read off the pi
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    # Set up channel list
+    global chan_list
+    chan_list = [13, 19, 26]  # 13 red, 19 green, 26 blue
+    GPIO.setup(chan_list, GPIO.OUT)
+
     # Resource tree creation
     root = resource.Site()
 
@@ -68,4 +83,5 @@ if __name__ == "__main__":
         main()
     except:
         print('Exiting program...')
+        GPIO.cleanup(chan_list)
         sys.exit(0)
