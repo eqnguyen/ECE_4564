8a7120f2e2b2556929b9731d5b6395d1ab2c289c
tlazor <tjlazor@vt.edu>
2017-04-14 19:27:39 -0400

    added cpu/network stats

--- Final_Project/backup_pi.py
+++ Final_Project/backup_pi.py
@@ -1,23 +1,44 @@
 #! /usr/bin/env python3
 
 import psutil
+import pickle
+
+import server_classes.py as RASD
 
 import asyncio
 import aiocoap
 import aiocoap.resource as resource
 
+
 class StatusResource(resource.Resource):
     def __init__(self):
         super(StatusResource, self).__init__()
+        cpu_percent = psutil.cpu_percent(interval=0)
+        network_io = psutil.net_io_counters(pernic=False)
+
+        net_stats = RASD.RASD_Net_Load(bytes_sent=network_io.bytes_sent,
+                                       bytes_recv=network_io.bytes_recv,
+                                       errin=network_io.errin,
+                                       errout=network_io.errout,
+                                       dropin=network_io.dropin,
+                                       dropout=network_io.dropout)
+
+        self.status = RASD.RASD_Status(cpu_percent=cpu_percent, net_stats=net_stats)
 
     async def render_get(self, request):
 
-        cpu_usage = psutil.cpu_percent(interval=0)
+        cpu_percent = psutil.cpu_percent(interval=0)
         network_io = psutil.net_io_counters(pernic=False)
 
+        net_stats = RASD.RASD_Net_Load(bytes_sent   =   network_io.bytes_sent,
+                                       bytes_recv   =   network_io.bytes_recv,
+                                       errin        =   network_io.errin,
+                                       errout       =   network_io.errout,
+                                       dropin       =   network_io.dropin,
+                                       dropout      =   network_io.dropout)
 
-        self.content = (pos.x, pos.y, pos.z, token)
-        return aiocoap.Message(payload=pickle.dumps(self.content))
+        self.status = RASD.RASD_Status(cpu_percent=cpu_percent, net_stats=net_stats)
+        return aiocoap.Message(payload=pickle.dumps(self.status))
 
 
 def main():
--- Final_Project/server_classes.py
+++ Final_Project/server_classes.py
@@ -21,6 +21,15 @@ class RASD_Backup(object):
 
 
 class RASD_Status(object):
-    def __init__(self, cpu_load, net_load):
-        self.cpu_load = cpu_load
+    def __init__(self, cpu_percent, net_load):
+        self.cpu_percent = cpu_percent
         self.net_load = net_load
+
+class RASD_Net_Load(object):
+    def __init__(self, bytes_sent, bytes_recv, errin, errout, dropin, dropout):
+        self.bytes_sent = bytes_sent
+        self.bytes_recv = bytes_recv
+        self.errin = errin
+        self.errout = errout
+        self.dropin = dropin
+        self.dropout = dropout
