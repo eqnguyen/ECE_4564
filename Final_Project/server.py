6fadc8fb4f9403964baa52b0cb0617986a3777b6
tlazor <tjlazor@vt.edu>
2017-04-16 00:21:24 -0400

    added rough tornado server code

--- Final_Project/server.py
+++ Final_Project/server.py
@@ -1,2 +1,20 @@
 #! /usr/bin/env python3
 
+import tornado.ioloop
+import tornado.web
+from tornado import template
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        t = template.Template("<html>{{ myvalue }}</html>")
+        self.write(t.generate(myvalue="XXX"))
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
