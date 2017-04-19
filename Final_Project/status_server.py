83ccc3f1a7fdd03a2b791a2f07bb8365f61e8db6
eqnguyen <eqnguyen@vt.edu>
2017-04-19 13:20:33 -0400

    Added LED controls to status_server

--- Final_Project/status_server.py
+++ Final_Project/status_server.py
@@ -4,6 +4,7 @@ import asyncio
 import pickle
 import sys
 
+import RPi.GPIO as GPIO
 import aiocoap
 import aiocoap.resource as resource
 import psutil
@@ -31,10 +32,30 @@ def get_RASD_Status():
                                    dropin=network_io.dropin,
                                    dropout=network_io.dropout)
 
+    # Change LED to indicate current status of the system
+    if cpu_percent < 25 and disk_usage < 50:
+        GPIO.output(chan_list, (False, True, False))
+    elif cpu_percent < 50 and disk_usage < 75:
+        GPIO.output(chan_list, (True, True, False))
+    else:
+        GPIO.output(chan_list, (True, False, False))
+
     return RASD.RASD_Status(cpu_percent=cpu_percent, net_stats=net_stats, disk_usage=disk_usage)
 
 
 def main():
+    # Set ping mode to the numbers you can read off the Pi
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    
+    # Set up the channel list
+    global chan_list
+    chan_list = [12, 16, 21]  # 12 red, 16 green, 21 blue
+    GPIO.setup(chan_list, GPIO.OUT)
+    
+    # Initialize LED to green
+    GPIO.output(chan_list, (False, True, False))
+    
     # Resource tree creation
     root = resource.Site()
 
@@ -48,7 +69,7 @@ def main():
 if __name__ == "__main__":
     try:
         main()
-    except Exception as err:
-        print(err)
+    except:
         print('Exiting program...')
+        GPIO.cleanup(chan_list)
         sys.exit(1)
