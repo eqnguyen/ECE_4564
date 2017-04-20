4d5a39bdc9d5464997caae39a4f8a36ba18c6e21
skralick <skralick@users.noreply.github.com>
2017-04-19 23:18:20 -0400

    made the instructions what i actually wanted

--- Final_Project/test_rsync_win/test_rsync_win.py
+++ Final_Project/test_rsync_win/test_rsync_win.py
@@ -1,18 +1,22 @@
 #! /usr/bin/env python3
 
 # Instructions:
-# 1. You should already have the bin folder in your Final_Project directory
+# 1. You should already have the bin folder in your test_rsync_win directory
 # 2. Add a testFile.txt you want to send
 # 3. Add a password.json file with rsync.ip and rsync.username (pi)
 # 4. Setup passwordless ssh - https://www.raspberrypi.org/documentation/remote-access/ssh/passwordless.md
-# 4a. go to bin directory in windows cmd prompt
+# 4a. go to test_rsync_win directory in windows cmd prompt
 # 4b. run cwrsync.cmd to configure ssh
-# 4c. run "ssh-keygen.exe -t rsa -C "some text" then press enter 4 times
-# 4d. Copy the text from bin/home/{user}/.ssh/id_rsa.pub to ~/.ssh/authorized_keys on your pi
-# 5. Run this script
+# 4c. go to the test_rsync_win/bin director in windows command prompt
+# 4d. run "ssh-keygen.exe -t rsa -C "some text" then press enter 4 times
+# 4e. Copy the text from test_rsync_win/home/{user}/.ssh/id_rsa.pub to ~/.ssh/authorized_keys on your pi
+# 5. You may have to start the rsync daemon on the pi with:
+# 		sudo systemctl enable rsync
+#		sudo systemctl start rsync
+# 6. Run this script (from win cmd prompt you can enter "python test_rsync_win.py")
 # 
-# Tip: You may need to run the rsync command once so the remote becomes
-# a known host
+# Tip: You may need to run the rsync command once (or type yes the 
+# first time you run the script) so the remote becomes a known host
 
 import json
 import sys
