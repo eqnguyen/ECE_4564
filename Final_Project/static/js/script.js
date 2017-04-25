e74eacbb7da35bf0e35d12cef41649576c5eb221
eqnguyen <eqnguyen@vt.edu>
2017-04-25 03:48:04 -0400

    reformmatted

--- Final_Project/static/js/script.js
+++ Final_Project/static/js/script.js
@@ -79,21 +79,21 @@ $(document).ready( function() {
 		} else {
 			$(".navigation").css("display", "none");
 			dropdown = false;
-		}
+        }
 	});
 
 	//Hide Hamburger Menu if click outside of menu
 	$('html').click(function(evt){
 		if(evt.target.id == "dropdown" || evt.target.id == "menuBar")
 			return;
-       //For descendants of menu_content being clicked
-       if($(evt.target).closest('#dropdown').length)
-       	return;
-       if($(evt.target).closest('#menuBar').length)
-       	return;
-      //Do processing of click event here for every element except with id menu_content
-      $(".navigation").css("display", "none");
-      dropdown = false;
+	    //For descendants of menu_content being clicked
+        if($(evt.target).closest('#dropdown').length)
+            return;
+        if($(evt.target).closest('#menuBar').length)
+            return;
+        //Do processing of click event here for every element except with id menu_content
+        $(".navigation").css("display", "none");
+        dropdown = false;
     });
 });
 
