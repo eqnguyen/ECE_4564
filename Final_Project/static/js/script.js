3c0aa655727107289f3a11b485b4d7462302085e
eqnguyen <eqnguyen@vt.edu>
2017-04-25 03:26:32 -0400

    Copied public files over to static directory for tornado server

--- Final_Project/static/js/script.js
+++ Final_Project/static/js/script.js
@@ -59,6 +59,8 @@ function serverResponded( data ) {
     }
 }
 
+var dropdown = false;
+
 $(document).ready( function() {
     /* get server ip address */
     ip = location.host;
@@ -67,6 +69,31 @@ $(document).ready( function() {
     $('button').click( function() {
         params = { op: "status" };
         $.getJSON( 'http://'+ ip + '/com' , params, serverResponded );
+    });
+
+    //Hamburger Menu show/hide
+	$( '#hamMenuPic' ).click(function() {
+		if(dropdown == false){
+			$(".navigation").css("display", "block");
+			dropdown = true;
+		} else {
+			$(".navigation").css("display", "none");
+			dropdown = false;
+		}
+	});
+
+	//Hide Hamburger Menu if click outside of menu
+	$('html').click(function(evt){
+		if(evt.target.id == "dropdown" || evt.target.id == "menuBar")
+			return;
+       //For descendants of menu_content being clicked
+       if($(evt.target).closest('#dropdown').length)
+       	return;
+       if($(evt.target).closest('#menuBar').length)
+       	return;
+      //Do processing of click event here for every element except with id menu_content
+      $(".navigation").css("display", "none");
+      dropdown = false;
     });
 });
 
