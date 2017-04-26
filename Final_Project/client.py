4f2ae5798076f6082d01f56e1bc1726234a313a6
eqnguyen <eqnguyen@vt.edu>
2017-04-26 11:00:23 -0400

    Client connects to server

--- Final_Project/client.py
+++ Final_Project/client.py
@@ -1,2 +1,31 @@
 #! /usr/bin/env python3
 
+# Client script for connecting to servers
+
+import socket
+import sys
+
+
+def main():
+    host = '172.29.76.116'
+    port = 50000
+    size = 1024
+    s = None
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        s.recv(size)
+    except socket.error as message:
+        if s:
+            s.close()
+        print("Unable to open socket: " + str(message))
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        print('Exiting program...')
+        sys.exit(0)
