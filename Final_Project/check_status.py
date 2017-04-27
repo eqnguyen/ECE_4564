e63f52fdb5c7bdb995d5139346cae6be00507536
eqnguyen <eqnguyen@vt.edu>
2017-04-27 04:16:56 -0400

    Put code in a main function

--- Final_Project/check_status.py
+++ Final_Project/check_status.py
@@ -15,11 +15,8 @@ import rasdrive_classes as RASD
 import requests
 from aiocoap import *
 
-
 server_list = [RASD.RASD_Server('rasdserver1'), RASD.RASD_Server('rasdserver2')]
 backup_list = [RASD.RASD_Backup('rasdbackup1'), RASD.RASD_Backup('rasdbackup2')]
-
-s = requests.Session()
 
 
 async def checkStatus(list, index):
@@ -46,30 +43,37 @@ async def checkStatus(list, index):
     list[index] = node
 
 
+def main():
+    global server_list
+    global backup_list
+
+    s = requests.Session()
+    loop = asyncio.get_event_loop()
+
+    while True:
+        tasks = [
+            asyncio.ensure_future(checkStatus(server_list, 0)),
+            asyncio.ensure_future(checkStatus(server_list, 1)),
+            asyncio.ensure_future(checkStatus(backup_list, 0)),
+            asyncio.ensure_future(checkStatus(backup_list, 1))
+        ]
+
+        loop.run_until_complete(asyncio.gather(*tasks))
+
+        # Post list of RasDrive nodes with updated statuses to web server every 5 seconds
+        payload = pickle.dumps({"server_list": server_list, "backup_list": backup_list})
+
+        try:
+            s.post("http://localhost:8888/com/status", data=payload)
+        except:
+            print('Could not post status to server')
+
+        sleep(5)
+
+
 if __name__ == "__main__":
     try:
-        loop = asyncio.get_event_loop()
-
-        while True:
-            tasks = [
-                asyncio.ensure_future(checkStatus(server_list, 0)),
-                asyncio.ensure_future(checkStatus(server_list, 1)),
-                asyncio.ensure_future(checkStatus(backup_list, 0)),
-                asyncio.ensure_future(checkStatus(backup_list, 1))
-            ]
-
-            loop.run_until_complete(asyncio.gather(*tasks))
-
-            # Post list of RasDrive nodes with updated statuses to web server every 5 seconds
-            payload = pickle.dumps({"server_list": server_list, "backup_list": backup_list})
-
-            try:
-                s.post("http://localhost:8888/com/status", data=payload)
-            except:
-                print('Could not post status to server')
-
-            sleep(5)
+        main()
     except:
         print('Exiting program...')
-        loop.close()
         sys.exit(1)
