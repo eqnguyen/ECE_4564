d65404674f9fa03e062b09d638d0bc8ef8b35758
eqnguyen <eqnguyen@vt.edu>
2017-05-02 20:26:53 -0400

    Added comments and changed sync order so all the files are the same in every place

--- Final_Project/client.py
+++ Final_Project/client.py
@@ -21,6 +21,8 @@ def get_ip():
     return ip
 
 
+# Sync files with backup nodes
+# All directories will be the same after sync
 def sync(ip_list):
     platform = sys.platform
     sync_command = ''
@@ -28,15 +30,18 @@ def sync(ip_list):
 
     for ip in ip_list:
         if platform == 'win32':
-            sync_command = './rsync/bin/rsync.exe -av -e ./rsync/bin/ssh pi@{ip}'.format(
-                ip=ip) + ':~/rsyncTests/ ./BackupDir/'
-            backup_command = './rsync/bin/rsync.exe -av -e ./rsync/bin/ssh ' \
-                             './BackupDir/ pi@{ip}'.format(ip=ip) + ':~/rsyncTests/'
+            sync_command = './rsync/bin/rsync.exe -av -e ./rsync/bin/ssh pi@{ip}:~/rsyncTests/ ./BackupDir/'.format(
+                ip=ip)
         elif platform == 'linux':
-            sync_command = 'rsync -av -e ssh pi@{ip]'.format(ip=ip) + ':~/rsyncTests/ ./BackupDir/'
-            backup_command = 'rsync -av -e ssh ./BackupDir/ pi@{ip}'.format(ip=ip) + ':~/rsyncTests/'
-
+            sync_command = 'rsync -av -e ssh pi@{ip]:~/rsyncTests/ ./BackupDir/'.format(ip=ip)
         call(sync_command.split(" "))
+
+    for ip in ip_list:
+        if platform == 'win32':
+            backup_command = './rsync/bin/rsync.exe -av -e ./rsync/bin/ssh ./BackupDir/ pi@{ip}:~/rsyncTests/'.format(
+                ip=ip)
+        elif platform == 'linux':
+            backup_command = 'rsync -av -e ssh ./BackupDir/ pi@{ip}:~/rsyncTests/'.format(ip=ip)
         call(backup_command.split(" "))
 
 
@@ -45,6 +50,7 @@ server_socket = None
 
 
 def main():
+    # Argument parser for server ip address
     parser = argparse.ArgumentParser()
     parser.add_argument("ip_address")
     args = parser.parse_args()
@@ -53,12 +59,13 @@ def main():
     global server_socket
 
     host = args.ip_address
-    conn_port = 50000
-    server_port = 25000
+    conn_port = 50000           # Connect to port 50000
+    server_port = 25000         # Listen on port 25000
     size = 1024
 
-    server_address = get_ip()
+    server_address = get_ip()   # Get ip address
 
+    # Connect to tcp server
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((host, conn_port))
@@ -69,6 +76,7 @@ def main():
         print("Unable to open socket: " + str(message))
         sys.exit(1)
 
+    # Bind socket and listen on it
     try:
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -83,11 +91,17 @@ def main():
 
     while True:
         try:
+            # Receive data from tcp server
             s.recv(size)
+
+            # Accept incoming connections on server socket
             client, address = server_socket.accept()
             client.settimeout(5)
+
+            # Receive sync command from web server
             (t, ip_list) = pickle.loads(client.recv(size))
 
+            # Sync based on command from server
             if t == 'Now':
                 print('\nSyncing now to:', ip_list)
                 sync(ip_list)
@@ -98,6 +112,8 @@ def main():
                 date = datetime.datetime(year, month, day, hour, minute)
                 delay = (date - datetime.datetime.now()).total_seconds()
                 print('\nSync scheduled for:', date, 'in', delay, 'seconds')
+
+                # Schedule sync at received time and data
                 Timer(delay, sync, kwargs={'ip_list': ip_list}).start()
 
             client.close()
